6375fb0da47f135f0a95b0dbd64951afa0fc8ea4
dls32208 <dls32208@gmail.com>
2023-04-07 06:42:25 +0000

    asdf

--- rss_news_uploader.py
+++ rss_news_uploader.py
@@ -8,7 +8,7 @@ ssh_agent = subprocess.Popen(['ssh-agent', '-s'], stdout=subprocess.PIPE)
 # ssh-add 실행
 subprocess.call('ssh-add ~/.ssh/id_rsa', shell=True)
 
-# 언론사별 RSS 피드 URL 설정
+# RSS 피드 URL 설정
 rss_urls = {
     "조선일보": "http://rssplus.chosun.com/",
     "동아일보": "http://rss.donga.com/",
@@ -17,7 +17,7 @@ rss_urls = {
     "한국경제": "http://rss.hankyung.com/",
     "경향신문": "http://www.khan.co.kr/help/help_rss.html",
     "한겨레": "http://www.hani.co.kr/arti/RSS/list1.html",
-    "연합뉴스tv": "https://www.yonhapnewstv.co.kr/add/rss",
+    "연합뉴스TV": "https://www.yonhapnewstv.co.kr/add/rss",
     "머니투데이": "https://www.mt.co.kr/mtm/mtm_rss.htm",
     "뉴시즈": "https://newsis.com/RSS/",
     "SBS": "https://news.sbs.co.kr/news/rss.do",
@@ -26,38 +26,42 @@ rss_urls = {
     "CNN": "http://edition.cnn.com/services/rss/"
 }
 
-# 저장할 파일 경로 설정
+# 저장할 파일 경로와 파일명 설정
 file_path = "/home/dls32208/Documents/VRChatKoreaNews"
+file_name = "news.html"
+
+# html 파일 생성
+def write_html():
+    with open(os.path.join(file_path, file_name), "w") as f:
+        f.write("<html>\n<head>\n<title>News</title>\n</head>\n<body>\n")
+        # 뉴스 기사 쓰기
+        for name, url in rss_urls.items():
+            feed = feedparser.parse(url)
+            f.write(f"<h1>{name}</h1>\n")
+            for entry in feed.entries:
+                f.write(f"<h2><a href='{entry.link}'>{entry.title}</a></h2>\n")
+                f.write(f"<p>{entry.summary}</p>\n\n")
+        f.write("</body>\n</html>")
+
+# 최초 실행 시 HTML 파일 생성
+write_html()
+
+# 깃허브에 업로드
+subprocess.call(f"cd {file_path} && git add {file_name} && git commit -m 'Update news' && git push", shell=True)
+
+# ssh-agent 종료
+ssh_agent.kill()
 
 # 3분마다 반복 실행
 while True:
+    time.sleep(180)
     # ssh-agent 실행
     ssh_agent = subprocess.Popen(['ssh-agent', '-s'], stdout=subprocess.PIPE)
     # ssh-add 실행
     subprocess.call('ssh-add ~/.ssh/id_rsa', shell=True)
 
-    # 모든 언론사 RSS 뉴스 기사 파싱
-    for media, rss_url in rss_urls.items():
-        # feedparser로 RSS 뉴스 기사 파싱
-        feed = feedparser.parse(rss_url)
+    # HTML 파일 생성
+    write_html()
 
-        # 저장할 파일 경로와 파일명 설정
-        file_name = f"{media}.html"
-
-        # html 파일 생성
-        with open(os.path.join(file_path, file_name), "w") as f:
-            f.write("<html>\n<head>\n<title>News</title>\n</head>\n<body>\n")
-            # 뉴스 기사 쓰기
-            for entry in feed.entries:
-                f.write(f"<h2><a href='{entry.link}'>{entry.title}</a></h2>\n")
-                f.write(f"<p>{entry.summary}</p>\n\n")
-            f.write("</body>\n</html>")
-
-        # 깃허브에 업로드
-        subprocess.call(f"cd {file_path} && git add {file_name} && git commit -m 'Update {media} news' && git push", shell=True)
-
-    # ssh-agent 종료
-    ssh_agent.kill()
-
-    # 3분마다 반복 실행
-    time.sleep(180)
+    # 깃허브에 업로드
+    subprocess
