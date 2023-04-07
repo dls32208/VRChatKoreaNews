4d6ce580e47f3a20ba2c07f58298fab6bfcdfeb0
dls32208 <dls32208@gmail.com>
2023-04-07 05:57:46 +0000

    Update rss_news_uploader.py

--- rss_news_uploader.py
+++ rss_news_uploader.py
@@ -1,6 +1,8 @@
+
 import feedparser
 import subprocess
 import os
+import time
 
 # ssh-agent 실행
 ssh_agent = subprocess.Popen(['ssh-agent', '-s'], stdout=subprocess.PIPE)
@@ -15,18 +17,46 @@ feed = feedparser.parse(rss_url)
 
 # 저장할 파일 경로와 파일명 설정
 file_path = "/home/dls32208/Documents/VRChatKoreaNews"
-file_name = "news.txt"
+file_name = "news.html"
 
-# 파일 열기
+# html 파일 생성
 with open(os.path.join(file_path, file_name), "w") as f:
+    f.write("<html>\n<head>\n<title>News</title>\n</head>\n<body>\n")
     # 뉴스 기사 쓰기
     for entry in feed.entries:
-        f.write(entry.title + "\n")
-        f.write(entry.link + "\n")
-        f.write(entry.summary + "\n\n")
+        f.write(f"<h2><a href='{entry.link}'>{entry.title}</a></h2>\n")
+        f.write(f"<p>{entry.summary}</p>\n\n")
+    f.write("</body>\n</html>")
 
 # 깃허브에 업로드
 subprocess.call(f"cd {file_path} && git add {file_name} && git commit -m 'Update news' && git push", shell=True)
 
 # ssh-agent 종료
 ssh_agent.kill()
+
+# 3분마다 반복 실행
+while True:
+    time.sleep(180)
+    # ssh-agent 실행
+    ssh_agent = subprocess.Popen(['ssh-agent', '-s'], stdout=subprocess.PIPE)
+    # ssh-add 실행
+    subprocess.call('ssh-add ~/.ssh/id_rsa', shell=True)
+
+    # feedparser로 RSS 뉴스 기사 파싱
+    feed = feedparser.parse(rss_url)
+
+    # html 파일 생성
+    with open(os.path.join(file_path, file_name), "w") as f:
+        f.write("<html>\n<head>\n<title>News</title>\n</head>\n<body>\n")
+        # 뉴스 기사 쓰기
+        for entry in feed.entries:
+            f.write(f"<h2><a href='{entry.link}'>{entry.title}</a></h2>\n")
+            f.write(f"<p>{entry.summary}</p>\n\n")
+        f.write("</body>\n</html>")
+
+    # 깃허브에 업로드
+    subprocess.call(f"cd {file_path} && git add {file_name} && git commit -m 'Update news' && git push", shell=True)
+
+    # ssh-agent 종료
+    ssh_agent.kill()
+
